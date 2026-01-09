56bdb5856a9b991f29289cbeb5b4463c3753825d
ann ningappa <annesh.svr@gmail.com>
2026-01-09 07:06:42 +0000

    added new order_d column

--- demo1/models/mart/fct_orders.sql
+++ demo1/models/mart/fct_orders.sql
@@ -1,46 +1,34 @@
-with 
-
-orders as (
-    
+with orders as (
     select * from {{ ref('stg_orders')}}
-
 ),
 
 order_items as (
-    
     select * from {{ ref('order_items')}}
-
 ),
 
 order_items_summary as (
-
     select
-
         order_items.order_id,
-
         sum(supply_cost) as order_cost,
         sum(is_food_item) as count_food_items,
         sum(is_drink_item) as count_drink_items
-
-
     from order_items
-
     group by 1
-
 ),
-
 
 compute_booleans as (
     select
-
         orders.*,
+
+        -- New derived column (BigQuery-safe)
+        DATE(orders.ordered_at) as order_d,
+
         count_food_items > 0 as is_food_order,
         count_drink_items > 0 as is_drink_order,
         order_cost
-
     from orders
-    
-    left join order_items_summary on orders.order_id = order_items_summary.order_id
+    left join order_items_summary
+        on orders.order_id = order_items_summary.order_id
 )
 
 select * from compute_booleans
